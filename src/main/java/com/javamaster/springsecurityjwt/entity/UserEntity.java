a4eba641336f58f7025ea8247f4535e8f561fe6a
Andrii <andriyslb@gmail.com>
2022-12-25 15:26:51 +0200

    upd

--- src/main/java/com/javamaster/springsecurityjwt/entity/UserEntity.java
+++ src/main/java/com/javamaster/springsecurityjwt/entity/UserEntity.java
@@ -12,8 +12,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "user_table")
-@Data
 public class UserEntity {
 
     @Id
@@ -29,4 +27,45 @@ public class UserEntity {
     @ManyToOne
     @JoinColumn(name = "role_id")
     private RoleEntity roleEntity;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(Integer id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public RoleEntity getRoleEntity() {
+        return roleEntity;
+    }
+
+    public void setRoleEntity(RoleEntity roleEntity) {
+        this.roleEntity = roleEntity;
+    }
 }
